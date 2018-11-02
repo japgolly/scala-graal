fb56eacc9b6dd88a8c8a6285567a4be20e57ede3
David Barri <japgolly@gmail.com>
2018-11-03 10:40:16 +1100

    Expr errors WIP - gtg

--- core/src/main/scala/japgolly/scalagraal/Expr.scala
+++ core/src/main/scala/japgolly/scalagraal/Expr.scala
@@ -1,82 +1,160 @@
 package japgolly.scalagraal
 
 import java.time.Duration
-import org.graalvm.polyglot.{Context, Source, TypeLiteral, Value}
+import org.graalvm.polyglot._
 import scala.reflect.ClassTag
 import scala.runtime.AbstractFunction1
-import scala.util.control.NonFatal
 
-final class Expr[A](private[Expr] val run: Context => A) extends AbstractFunction1[Context, Expr.Result[A]] {
-  override def apply(context: Context): Expr.Result[A] =
-    try
-      Right(run(context))
-    catch {
-      case NonFatal(t) => Left(t)
-    }
+object Example {
+  val x = Expr(???).asString
+}
 
-  def map[B](f: A => B): Expr[B] =
+final class Expr[E, A](private[Expr] val run: Context => Expr.Result[E, A]) extends AbstractFunction1[Context, Expr.Result[E, A]] {
+
+  override def apply(context: Context): Expr.Result[E, A] =
+    run(context)
+
+  def map[B](f: A => B): Expr[E, B] =
+    new Expr(run(_).map(f))
+
+  def emap[B](f: A => Either[E, B]): Expr[E, B] =
+    new Expr(run(_).flatMap(f))
+
+  def eflatmap[F, B](f: Either[E, A] => Either[F, B]): Expr[F, B] =
     new Expr(f compose run)
 
-  def flatMap[B](f: A => Expr[B]): Expr[B] =
-    new Expr(c => f(run(c)).run(c))
+  def flatMap[B](f: A => Expr[E, B]): Expr[E, B] =
+    new Expr(c => run(c).flatMap(f(_).run(c)))
 
-  // @throws ClassCastException if this value could not be converted to string.
-  // @throws UnsupportedOperationException if this value does not represent a string.
-  // @throws PolyglotException if a guest language error occurred during execution.
-  def asBoolean(implicit ev: Expr[A] =:= Expr[Value]): Expr[Boolean] = ev(this).map(_.asBoolean())
-  def asByte   (implicit ev: Expr[A] =:= Expr[Value]): Expr[Byte   ] = ev(this).map(_.asByte())
-  def asDouble (implicit ev: Expr[A] =:= Expr[Value]): Expr[Double ] = ev(this).map(_.asDouble())
-  def asFloat  (implicit ev: Expr[A] =:= Expr[Value]): Expr[Float  ] = ev(this).map(_.asFloat())
-  def asInt    (implicit ev: Expr[A] =:= Expr[Value]): Expr[Int    ] = ev(this).map(_.asInt())
-  def asLong   (implicit ev: Expr[A] =:= Expr[Value]): Expr[Long   ] = ev(this).map(_.asLong())
-  def asShort  (implicit ev: Expr[A] =:= Expr[Value]): Expr[Short  ] = ev(this).map(_.asShort())
-  def asString (implicit ev: Expr[A] =:= Expr[Value]): Expr[String ] = ev(this).map(_.asString())
+//  // @throws ClassCastException if this value could not be converted to string.
+//  // @throws UnsupportedOperationException if this value does not represent a string.
+//  // @throws PolyglotException if a guest language error occurred during execution.
+//  def asBoolean(implicit ev: Expr[E, A] =:= Expr[E, Value]): Expr[E, Boolean] = ev(this).map(_.asBoolean())
+//  def asByte   (implicit ev: Expr[E, A] =:= Expr[E, Value]): Expr[E, Byte   ] = ev(this).map(_.asByte())
+//  def asDouble (implicit ev: Expr[E, A] =:= Expr[E, Value]): Expr[E, Double ] = ev(this).map(_.asDouble())
+//  def asFloat  (implicit ev: Expr[E, A] =:= Expr[E, Value]): Expr[E, Float  ] = ev(this).map(_.asFloat())
+//  def asInt    (implicit ev: Expr[E, A] =:= Expr[E, Value]): Expr[E, Int    ] = ev(this).map(_.asInt())
+//  def asLong   (implicit ev: Expr[E, A] =:= Expr[E, Value]): Expr[E, Long   ] = ev(this).map(_.asLong())
+//  def asShort  (implicit ev: Expr[E, A] =:= Expr[E, Value]): Expr[E, Short  ] = ev(this).map(_.asShort())
+//  def asString (implicit ev: Expr[E, A] =:= Expr[E, Value]): Expr[E, String ] = ev(this).map(_.asString())
 
-  def as[T](t: TypeLiteral[T])(implicit ev: Expr[A] =:= Expr[Value]): Expr[T] =
-    ev(this).map(_.as(t))
+  def asString(implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead]): Expr[m.C, String] =
+    ev(this).eflatmap {
+      case Right(v) =>
+        try
+          Right(v.asString())
+        catch {
+          case e: ClassCastException            => Left(m.b(ExprError.ValueCastError(v, e)))
+          case e: UnsupportedOperationException => Left(m.b(ExprError.ValueReprError(v, e)))
+          case e: PolyglotException             => Left(m.b(ExprError.GuestLangError(v, e)))
+        }
+      case Left(e) =>
+        Left(m.a(e))
+    }
 
-  def as[T](implicit ev: Expr[A] =:= Expr[Value], ct: ClassTag[T]): Expr[T] = {
-    val t = ct.runtimeClass.asInstanceOf[Class[T]]
-    ev(this).map(_.as(t))
-  }
-
-  def asOption[B](f: Expr[Value] => Expr[B])(implicit ev: Expr[A] =:= Expr[Value]): Expr[Option[B]] = {
-    val self = ev(this)
-    new Expr(c => {
-      val v = self.run(c)
-      if (v.isNull) None else Some(f(Expr.const(v)).run(c))
-    })
-  }
-
-  def timed: Expr[(Duration, A)] =
-    new Expr(ctx => {
-      val start = System.nanoTime()
-      val a = run(ctx)
-      val end = System.nanoTime()
-      val dur = Duration.ofNanos(end - start)
-      (dur, a)
-    })
+//  def as[T](t: TypeLiteral[T])(implicit ev: Expr[E, A] =:= Expr[E, Value]): Expr[E, T] =
+//    ev(this).map(_.as(t))
+//
+//  def as[T](implicit ev: Expr[E, A] =:= Expr[E, Value], ct: ClassTag[T]): Expr[E, T] = {
+//    val t = ct.runtimeClass.asInstanceOf[Class[T]]
+//    ev(this).map(_.as(t))
+//  }
+//
+//  def asOption[B](f: Expr[E, Value] => Expr[E, B])(implicit ev: Expr[E, A] =:= Expr[E, Value]): Expr[E, Option[B]] = {
+//    val self = ev(this)
+//    new Expr(c => {
+//      val v = self.run(c)
+//      if (v.isNull) None else Some(f(Expr.const(v)).run(c))
+//    })
+//  }
+//
+//  def timed: Expr[E, (Duration, A)] =
+//    new Expr(ctx => {
+//      val start = System.nanoTime()
+//      val a = run(ctx)
+//      val end = System.nanoTime()
+//      val dur = Duration.ofNanos(end - start)
+//      (dur, a)
+//    })
 }
 
 object Expr {
-  type Error = Throwable
-  type Result[+A] = Either[Error, A]
+  type Result[E, A] = Either[E, A]
 
-  def apply(js: CharSequence): Expr[Value] =
-    apply(Source.create("js", js))
+  def apply(source: Source): Expr[ExprError.InEval, Value] =
+    new Expr(c =>
+      try
+        Right(c.eval(source))
+      catch {
+        case e: PolyglotException => Left(ExprError.EvalError(e))
+        case e: IllegalStateException => Left(ExprError.ContextClosed(e))
+        case e: IllegalArgumentException => Left(ExprError.UnsupportedLanguageOrMimeType(e))
+      }
+    )
 
-  def apply(source: Source): Expr[Value] = {
-    // @throws PolyglotException in case parsing or evaluation of the guest language code failed.
-    // @throws IllegalStateException if the context is already closed, the current thread is not
-    //             allowed to access this context
-    // @throws IllegalArgumentException if the language of the given source is not installed or the
-    //             {@link Source#getMimeType() MIME type} is not supported with the language.
-    new Expr(_.eval(source))
+  def const[A](a: A): Expr[Nothing, A] = {
+    val r = Right(a)
+    new Expr(_ => r)
   }
 
-  def const[A](a: A): Expr[A] =
-    new Expr(_ => a)
+  def point[A](a: => A): Expr[Nothing, A] =
+    new Expr(_ => Right(a))
+}
 
-  def point[A](a: => A): Expr[A] =
-    new Expr(_ => a)
+object ExprError {
+
+  sealed trait InEvalOrRead
+
+  sealed trait InEval extends InEvalOrRead
+
+  final case class EvalError(underlying: PolyglotException) extends InEval
+  final case class ContextClosed(underlying: IllegalStateException) extends InEval
+  final case class UnsupportedLanguageOrMimeType(underlying: IllegalArgumentException) extends InEval
+
+  sealed trait InRead extends InEvalOrRead {
+    val value: Value
+    val underlying: Throwable
+  }
+
+  final case class ValueIsNull(value: Value, underlying: NullPointerException) extends InRead
+
+  /** the value could not be converted to the expected type */
+  final case class ValueCastError(value: Value, underlying: ClassCastException) extends InRead
+
+  /** the value does not represent the expected type */
+  final case class ValueReprError(value: Value, underlying: UnsupportedOperationException) extends InRead
+
+  /** a guest language error occurred during execution */
+  final case class GuestLangError(value: Value, underlying: PolyglotException) extends InRead
+
+  trait Merge[-A, -B] {
+    type C
+    val a: A => C
+    val b: B => C
+  }
+  object Merge {
+    type Aux[A, B, CC] = Merge[A, B] { type C = CC }
+    def apply[A, B, CC](x: A => CC, y: B => CC): Aux[A, B, CC] = new Merge[A, B] { type C = CC; val a = x; val b = y}
+
+    implicit def lub[A <: AnyRef]: Aux[A, A, A] = Merge(a => a, a => a)
+  }
+
+//  trait Merge[A, B] {
+//    type C
+//    val a: A => C
+//    val b: B => C
+//  }
+//  trait MergePri2 {
+//    implicit def lub[A, B <: A, C <: A]: Merge.Aux[B, C, A] = Merge(a => a, a => a)
+//  }
+//  trait MergePri1 extends MergePri2 {
+//    implicit def lubL[A, B <: A]: Merge.Aux[A, B, A] = Merge(a => a, a => a)
+//    implicit def lubR[A, B >: A]: Merge.Aux[A, B, B] = Merge(a => a, a => a)
+//  }
+//  object Merge extends MergePri1 {
+//    type Aux[A, B, CC] = Merge[A, B] { type C = CC }
+//    def apply[A, B, CC](x: A => CC, y: B => CC): Aux[A, B, CC] = new Merge[A, B] { type C = CC; val a = x; val b = y}
+//
+//    implicit def refl[A]: Aux[A, A, A] = Merge(a => a, a => a)
+//  }
 }
