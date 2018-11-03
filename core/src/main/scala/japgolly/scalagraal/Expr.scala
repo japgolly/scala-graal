5e1ef513c35ad5722f8918b8ff15b507dd679561
David Barri <japgolly@gmail.com>
2018-11-03 16:41:13 +1100

    Until Scala 3 this will do

--- core/src/main/scala/japgolly/scalagraal/Expr.scala
+++ core/src/main/scala/japgolly/scalagraal/Expr.scala
@@ -5,159 +5,131 @@ import org.graalvm.polyglot._
 import scala.reflect.ClassTag
 import scala.runtime.AbstractFunction1
 
-object Example {
-  val x = Expr(???).asString
-  x: Expr[ExprError.InEvalOrRead, String]
-  val y = x.flatMap(_ => Expr(???).asInt)
-  y: Expr[ExprError.InEvalOrRead, Int]
-}
+final class Expr[A](private[Expr] val run: Context => A) extends AbstractFunction1[Context, Expr.Result[A]] {
 
-final class Expr[E, A](private[Expr] val run: Context => Expr.Result[E, A]) extends AbstractFunction1[Context, Expr.Result[E, A]] {
-
-  override def apply(context: Context): Expr.Result[E, A] =
-    run(context)
-
-  def map[B](f: A => B): Expr[E, B] =
-    new Expr(run(_).map(f))
-
-  def emap[B](f: A => Either[E, B]): Expr[E, B] =
-    new Expr(run(_).flatMap(f))
-
-  def eflatmap[F, B](f: Either[E, A] => Either[F, B]): Expr[F, B] =
-    new Expr(f compose run)
-
-  def flatMap[B](f: A => Expr[E, B]): Expr[E, B] =
-    new Expr(c => run(c).flatMap(f(_).run(c)))
-//  def flatMap[F, B](f: A => Expr[F, B])(implicit m: ExprError.Merge[E, F]): Expr[m.Out, B] =
-//    new Expr(c => run(c) match {
-//      case Right(a) => f(a).run(c) match {
-//        case Right(b) => Right(b)
-//        case Left(e) => Left(m.b(e))
-//      }
-//      case Left(e) => Left(m.a(e))
-//    })
-
-  def leftMap[F](f: E => F): Expr[F, A] =
-    new Expr(run(_).left.map(f))
-
-  def leftWiden[F >: E]: Expr[F, A] =
-    this.asInstanceOf[Expr[F, A]]
-
-  private def _as[B](f: Value => B)(implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead]): Expr[m.Out, B] =
-    ev(this).eflatmap {
-      case Right(v) =>
-        try
-          Right(f(v))
-        catch {
-          case e: ClassCastException            => Left(m.b(ExprError.ValueCastError(v, e)))
-          case e: NullPointerException          => Left(m.b(ExprError.ValueIsNull(v, e)))
-          case e: UnsupportedOperationException => Left(m.b(ExprError.ValueReprError(v, e)))
-          case e: PolyglotException             => Left(m.b(ExprError.GuestLangError(v, e)))
-        }
-      case Left(e) =>
-        Left(m.a(e))
+  override def apply(context: Context): Expr.Result[A] =
+    try
+      Right(run(context))
+    catch {
+      case t: ExprError => Left(t)
+      case t: Throwable => throw t
     }
 
-  def asBoolean(implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead]): Expr[m.Out, Boolean] = _as(_.asBoolean())(ev, m)
-  def asByte   (implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead]): Expr[m.Out, Byte   ] = _as(_.asByte())(ev, m)
-  def asDouble (implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead]): Expr[m.Out, Double ] = _as(_.asDouble())(ev, m)
-  def asFloat  (implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead]): Expr[m.Out, Float  ] = _as(_.asFloat())(ev, m)
-  def asInt    (implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead]): Expr[m.Out, Int    ] = _as(_.asInt())(ev, m)
-  def asLong   (implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead]): Expr[m.Out, Long   ] = _as(_.asLong())(ev, m)
-  def asShort  (implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead]): Expr[m.Out, Short  ] = _as(_.asShort())(ev, m)
-  def asString (implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead]): Expr[m.Out, String ] = _as(_.asString())(ev, m)
+  def map[B](f: A => B): Expr[B] =
+    new Expr(f compose run)
 
-  def as[T](t: TypeLiteral[T])(implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead]): Expr[m.Out, T] =
-    _as(_.as(t))(ev, m)
+  def flatMap[B](f: A => Expr[B]): Expr[B] =
+    new Expr(c => f(run(c)).run(c))
 
-  def as[T](implicit ev: Expr[E, A] =:= Expr[E, Value], m: ExprError.Merge[E, ExprError.InRead], ct: ClassTag[T]): Expr[m.Out, T] = {
+  @inline private def _as[B](f: Value => B)(implicit ev: Expr[A] =:= Expr[Value]): Expr[B] =
+    ev(this).map(v => ExprError.InResult.capture(v, f))
+
+  def asBoolean(implicit ev: Expr[A] =:= Expr[Value]): Expr[Boolean] = _as(_.asBoolean())
+  def asByte   (implicit ev: Expr[A] =:= Expr[Value]): Expr[Byte   ] = _as(_.asByte())
+  def asDouble (implicit ev: Expr[A] =:= Expr[Value]): Expr[Double ] = _as(_.asDouble())
+  def asFloat  (implicit ev: Expr[A] =:= Expr[Value]): Expr[Float  ] = _as(_.asFloat())
+  def asInt    (implicit ev: Expr[A] =:= Expr[Value]): Expr[Int    ] = _as(_.asInt())
+  def asLong   (implicit ev: Expr[A] =:= Expr[Value]): Expr[Long   ] = _as(_.asLong())
+  def asShort  (implicit ev: Expr[A] =:= Expr[Value]): Expr[Short  ] = _as(_.asShort())
+  def asString (implicit ev: Expr[A] =:= Expr[Value]): Expr[String ] = _as(_.asString())
+
+  def as[T](t: TypeLiteral[T])(implicit ev: Expr[A] =:= Expr[Value]): Expr[T] =
+    _as(_.as(t))
+
+  def as[T](implicit ev: Expr[A] =:= Expr[Value], ct: ClassTag[T]): Expr[T] = {
     val t = ct.runtimeClass.asInstanceOf[Class[T]]
-    _as(_.as(t))(ev, m)
+    _as(_.as(t))
   }
 
-  def asOption[F, B](f: Expr[E, Value] => Expr[F, B])
-                    (implicit ev: Expr[E, A] =:= Expr[E, Value],
-                     m1: ExprError.Merge[E, F],
-                     m2: ExprError.Merge[m1.Out, ExprError.InRead]): Expr[m2.Out, Option[B]] = {
+  def asOption[F, B](f: Expr[Value] => Expr[B])(implicit ev: Expr[A] =:= Expr[Value]): Expr[Option[B]] = {
     val self = ev(this)
-    new Expr(c =>
-      self.run(c) match {
-        case Right(v) =>
-          if (v.isNull) Right(None) else f(Expr.const(v).leftWiden[E]).run(c).map(Some(_))
+    new Expr(c => {
+      val v = self.run(c)
+      if (ExprError.InResult.capture(v, _.isNull))
+        None
+      else
+        Some(f(Expr.const(v)).run(c))
     })
   }
 
-//  def timed: Expr[E, (Duration, A)] =
-//    new Expr(ctx => {
-//      val start = System.nanoTime()
-//      val a = run(ctx)
-//      val end = System.nanoTime()
-//      val dur = Duration.ofNanos(end - start)
-//      (dur, a)
-//    })
+  def timed: Expr[(Duration, A)] =
+    new Expr(ctx => {
+      val start = System.nanoTime()
+      val a = run(ctx)
+      val end = System.nanoTime()
+      val dur = Duration.ofNanos(end - start)
+      (dur, a)
+    })
 }
 
 object Expr {
-  type Result[E, A] = Either[E, A]
+  type Result[A] = Either[ExprError, A]
 
-  def apply(source: Source): Expr[ExprError.InEval, Value] =
-    new Expr(c =>
-      try
-        Right(c.eval(source))
-      catch {
-        case e: PolyglotException => Left(ExprError.EvalError(e))
-        case e: IllegalStateException => Left(ExprError.ContextClosed(e))
-        case e: IllegalArgumentException => Left(ExprError.UnsupportedLanguageOrMimeType(e))
-      }
-    )
+  def apply(source: Source): Expr[Value] =
+    new Expr(c => ExprError.InEval.capture(c.eval(source)))
 
-  def const[A](a: A): Expr[Nothing, A] = {
-    val r = Right(a)
-    new Expr(_ => r)
-  }
+  def const[A](a: A): Expr[A] =
+    new Expr(_ => a)
 
-  def point[A](a: => A): Expr[Nothing, A] =
-    new Expr(_ => Right(a))
+  def point[A](a: => A): Expr[A] =
+    new Expr(_ => a)
+}
+
+sealed trait ExprError extends RuntimeException {
+  val underlying: Throwable
 }
 
 object ExprError {
 
-  sealed trait InEvalOrRead
+  // ===================================================================================================================
 
-  sealed trait InEval extends InEvalOrRead
+  sealed trait InEval extends ExprError
+
+  object InEval {
+    def capture[A](a: => A): A =
+      try a catch { case t: Throwable => throw unapply(t).getOrElse(t) }
+
+    def unapply(t: Throwable): Option[InEval] = t match {
+      case t: InEval                        => Some(t)
+      case t: IllegalArgumentException      => Some(ExprError.UnsupportedLanguageOrMimeType(t))
+      case t: IllegalStateException         => Some(ExprError.ContextClosed(t))
+      case t: PolyglotException             => Some(ExprError.EvalError(t))
+    }
+  }
 
   final case class EvalError(underlying: PolyglotException) extends InEval
+
   final case class ContextClosed(underlying: IllegalStateException) extends InEval
+
   final case class UnsupportedLanguageOrMimeType(underlying: IllegalArgumentException) extends InEval
 
-  sealed trait InRead extends InEvalOrRead {
+  // ===================================================================================================================
+
+  sealed trait InResult extends ExprError {
     val value: Value
-    val underlying: Throwable
   }
 
-  final case class ValueIsNull(value: Value, underlying: NullPointerException) extends InRead
+  object InResult {
+    def capture[A](v: Value, f: Value => A): A =
+      try f(v) catch { case t: Throwable => throw unapply(t).fold(t)(_(v)) }
 
-  /** the value could not be converted to the expected type */
-  final case class ValueCastError(value: Value, underlying: ClassCastException) extends InRead
-
-  /** the value does not represent the expected type */
-  final case class ValueReprError(value: Value, underlying: UnsupportedOperationException) extends InRead
+    def unapply(t: Throwable): Option[Value => InResult] = t match {
+      case t: InResult                      => Some(_ => t)
+      case t: PolyglotException             => Some(ExprError.ValueError(_, t))
+      case t: NullPointerException          => Some(ExprError.ValueIsNull(_, t))
+      case t: ClassCastException            => Some(ExprError.ValueCastError(_, t))
+      case t: UnsupportedOperationException => Some(ExprError.ValueReprError(_, t))
+    }
+  }
 
   /** a guest language error occurred during execution */
-  final case class GuestLangError(value: Value, underlying: PolyglotException) extends InRead
+  final case class ValueError(value: Value, underlying: PolyglotException) extends InResult
 
-  trait Merge[-A, -B] {
-    type Out
-    val a: A => Out
-    val b: B => Out
-  }
-  trait MergeFallback {
-    implicit def coproduct[A, B]: Merge.Aux[A, B, Either[A, B]] = Merge(Left(_), Right(_))
-  }
-  object Merge {
-    type Aux[A, B, O] = Merge[A, B] { type Out = O }
-    def apply[A, B, O](x: A => O, y: B => O): Aux[A, B, O] = new Merge[A, B] { type Out = O; val a = x; val b = y}
+  final case class ValueIsNull(value: Value, underlying: NullPointerException) extends InResult
 
-    implicit def lub[A <: AnyRef]: Aux[A, A, A] = Merge(a => a, a => a)
-  }
+  /** the value could not be converted to the expected type */
+  final case class ValueCastError(value: Value, underlying: ClassCastException) extends InResult
+
+  /** the value does not represent the expected type */
+  final case class ValueReprError(value: Value, underlying: UnsupportedOperationException) extends InResult
 }
