35023f9670da1b9bd01e8c0b90132272429480c3
David Barri <japgolly@gmail.com>
2018-11-13 18:47:06 +1100

    Replace ContextSync

--- core/src/main/scala/japgolly/scalagraal/ContextSync.scala
+++ core/src/main/scala/japgolly/scalagraal/ContextSync.scala
@@ -1,83 +1,163 @@
 package japgolly.scalagraal
 
-import org.graalvm.polyglot.Context
+import org.graalvm.polyglot.{Context, Engine}
 
 trait ContextSync {
-  def eval[A](f: Context => A): A
-  def withAround(f: ContextSync.Around): ContextSync
+  def eval[A](f: Expr[A]): Expr.Result[A]
 }
 
 object ContextSync {
 
-  def apply()(implicit lang: Language): ContextSync =
-    apply(mutex = true)
+  def apply()(implicit l: Language): ContextSync = fixedContext()
 
-  def apply(mutex: Boolean)(implicit lang: Language): ContextSync =
-    apply(Context.create(lang.name), mutex)
+  def fixedContext()(implicit l: Language): ContextSync = Builder.fixedContext().build()
+  def fixedContext(c: Context): ContextSync = Builder.fixedContext(c).build()
+  def newContextPerUse()(implicit l: Language): ContextSync = Builder.newContextPerUse().build()
+  def newContextPerUse(c: => Context): ContextSync = Builder.newContextPerUse(c).build()
+  def newContextPerUse(f: Engine => Context): ContextSync = Builder.newContextPerUse(f).build()
 
-  def apply(context: Context, mutex: Boolean = true): ContextSync =
-    if (mutex)
-      new SingleBlocking(context, Around.id, new AnyRef)
-    else
-      new SingleThreaded(context, Around.id)
+  object Builder {
 
-  private class SingleBlocking(context: Context, around: Around, lock: AnyRef) extends ContextSync {
-    override def eval[A](f: Context => A) =
-      lock.synchronized {
-        around.enterAndLeave(context, f)
-      }
+    def fixedContext()(implicit l: Language): Builder =
+      fixedContext(Context.create(l.name))
 
-    override def withAround(f: ContextSync.Around) =
-      new SingleBlocking(context, around.insideOf(f), lock)
-  }
+    def fixedContext(c: Context): Builder =
+      start(Right(c), useMutex = true)
 
-  private class SingleThreaded(context: Context, around: Around) extends ContextSync {
-    override def eval[A](f: Context => A) =
-      around.enterAndLeave(context, f)
+    def newContextPerUse()(implicit l: Language): Builder =
+      newContextPerUse(Context.newBuilder(l.name).engine(_).build())
 
-    override def withAround(f: ContextSync.Around) =
-      new SingleThreaded(context, around.insideOf(f))
-  }
+    def newContextPerUse(c: => Context): Builder =
+      start(Left(() => c), useMutex = false)
 
-  // ===================================================================================================================
-
-  trait Around { inner =>
-    def apply[A](c: Context, f: Context => A): A
-
-    def insideOf(outer: Around): Around = new Around {
-      override def apply[A](c: Context, f: Context => A) =
-        outer(c, inner(_, f))
+    def newContextPerUse(f: Engine => Context): Builder = {
+      val e = Engine.create()
+      start(Left(() => f(e)), useMutex = false)
     }
 
-    final def enterAndLeave[A](c: Context, f: Context => A): A =
+    private def start(c: Either[() => Context, Context], useMutex: Boolean): Builder =
+      new Builder(c, _useMutex = useMutex, None, None, None, None, None)
+
+    private[Builder] val close: Context => Unit = _.close(true)
+    private[Builder] val dontClose: Context => Unit = _ => ()
+  }
+
+  final class Builder(_ctxProvider: Either[() => Context, Context],
+                      _useMutex: Boolean,
+                      _afterCreate: Option[Expr[_]],
+                      _beforeEval: Option[Expr[_]],
+                      _afterEval: Option[Expr[_]],
+                      _preClose: Option[Expr[_]],
+                      _metricWriter: Option[ContextMetrics.Writer]) {
+    //                      _metricsDims: List[Metrics.Dimension]) {
+
+    private def copy(_ctxProvider: Either[() => Context, Context] = _ctxProvider,
+                     _useMutex: Boolean = _useMutex,
+                     _afterCreate: Option[Expr[_]] = _afterCreate,
+                     _beforeEval: Option[Expr[_]] = _beforeEval,
+                     _afterEval: Option[Expr[_]] = _afterEval,
+                      _preClose: Option[Expr[_]] = _preClose,
+                     _metricWriter: Option[ContextMetrics.Writer] = _metricWriter): Builder =
+      new Builder(_ctxProvider, _useMutex, _afterCreate, _beforeEval, _afterEval, _preClose, _metricWriter)
+
+    def useMutex(b: Boolean): Builder =
+      copy(_useMutex = b)
+
+    def afterContextCreate(e: Expr[_]): Builder =
+      copy(_afterCreate = Some(_afterCreate.fold[Expr[_]](e)(_ >> e)))
+
+    def beforeEval(e: Expr[_]): Builder =
+      copy(_beforeEval = Some(_beforeEval.fold[Expr[_]](e)(_ >> e)))
+
+    def afterEval(e: Expr[_]): Builder =
+      copy(_afterEval = Some(_afterEval.fold[Expr[_]](e)(_ >> e)))
+
+    def beforeContextClose(e: Expr[_]): Builder =
+      copy(_preClose = Some(_preClose.fold[Expr[_]](e)(_ >> e)))
+
+    def writeMetrics(w: ContextMetrics.Writer): Builder =
+      copy(_metricWriter = Some(_metricWriter.fold(w)(_ >> w)))
+
+    def build(): ContextSync =
+      new Impl(
+        useMutex = _useMutex,
+        getCtx = _ctxProvider.fold(identity, () => _),
+        beforeEval = _beforeEval.getOrElse(Expr.unit),
+        afterEval = _afterEval.getOrElse(Expr.unit),
+        closeCtx = if (_ctxProvider.isLeft) Builder.close else Builder.dontClose,
+        metricWriter = _metricWriter.getOrElse(ContextMetrics.Noop))
+  }
+
+  private final class Impl(useMutex: Boolean,
+                           getCtx: () => Context,
+                           beforeEval: Expr[_],
+                           afterEval: Expr[_],
+                           closeCtx: Context => Unit,
+                           metricWriter: ContextMetrics.Writer) extends ContextSync {
+
+    private[this] val lock: AnyRef =
+      if (useMutex) new AnyRef else null
+
+    override def eval[A](expr: Expr[A]): Expr.Result[A] = {
+      val timerTotal = DurationLite.start()
+      var durWaited, durPre, durEval, durPost = DurationLite.Zero
+      var afterEvalResult: Expr.Result[_] = null
       try {
-        c.enter()
-        this(c, f)
-      } finally
-        c.leave()
-  }
+        val resultFn = () => {
+          durWaited = timerTotal.stop()
+          // ----------------------------------------------------
+          val timerPre = DurationLite.start()
+          var timerPost = timerPre // this will be set before used
+          val ctx = getCtx()
+          try {
+            ctx.enter()
+            try {
+              val resultBefore = beforeEval(ctx)
+              durPre = timerPre.stop()
+              if (resultBefore.isLeft) {
+                resultBefore.asInstanceOf[Expr.Result[A]]
+              } else {
+                // ----------------------------------------------------
+                try {
+                  val timerEval = DurationLite.start()
+                  val result = expr(ctx)
+                  durEval = timerEval.stop()
+                  result
+                  // ----------------------------------------------------
+                } finally {
+                  timerPost = DurationLite.start()
+                  afterEvalResult = afterEval(ctx)
+                }
+              }
 
-  object Around {
-    val id: Around = new Around {
-      override def apply[A](c: Context, f: Context => A) = f(c)
+            } finally {
+              ctx.leave()
+            }
+          } finally {
+            closeCtx(ctx)
+            durPost = timerPost.stop()
+          }
+          // ----------------------------------------------------
+        }
+
+        val result =
+          if (lock eq null) resultFn() else lock.synchronized(resultFn())
+
+        if ((afterEvalResult ne null) && afterEvalResult.isLeft && result.isRight)
+          afterEvalResult.asInstanceOf[Expr.Result[A]]
+        else
+          result
+
+      } finally {
+        val durTotal = timerTotal.stop()
+        metricWriter(
+          waited = durWaited,
+          pre = durPre,
+          eval = durEval,
+          post = durPost,
+          total = durTotal)
+      }
     }
-
-    def before(h: Context => Unit): Around =
-      new Around {
-        override def apply[A](c: Context, f: Context => A): A = {
-          h(c)
-          f(c)
-        }
-      }
-
-    def after(h: Context => Unit): Around =
-      new Around {
-        override def apply[A](c: Context, f: Context => A): A = {
-          val a = f(c)
-          h(c)
-          a
-        }
-      }
   }
 }
 
