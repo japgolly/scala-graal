f9623ab1e05b8683c93705a72ae929072c71d1f3
David Barri <japgolly@gmail.com>
2019-06-01 18:25:00 +1000

    hmmmm

--- build.sbt
+++ build.sbt
@@ -1,8 +1,5 @@
 name := "scala-graal"
 
 val root            = ScalaGraal.root
-val core            = ScalaGraal.core
 val extBoopickleJS  = ScalaGraal.extBoopickleJS
 val extBoopickleJVM = ScalaGraal.extBoopickleJVM
-val extPrometheus   = ScalaGraal.extPrometheus
-val benchmark       = ScalaGraal.benchmark
--- project/Build.scala
+++ project/Build.scala
@@ -20,11 +20,9 @@ object ScalaGraal {
 
   object Ver {
     final val BooPickle     = "1.3.1"
-    final val Graal         = "19.0.0"
     final val KindProjector = "0.9.10"
     final val Microlibs     = "1.20"
     final val MTest         = "0.6.7"
-    final val Prometheus    = "0.6.0"
     final val Scala211      = "2.11.12"
     final val Scala212      = "2.12.8"
   }
@@ -71,46 +69,22 @@ object ScalaGraal {
 
   // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-  lazy val genExprBoilerplate = TaskKey[File]("genExprBoilerplate")
-
   lazy val root =
     Project("root", file("."))
       .configure(commonSettings.jvm, preventPublication)
       .aggregate(
-        core,
-        extBoopickleJVM, extBoopickleJS, extPrometheus,
-        benchmark)
-
-  lazy val core = project
-    .configure(commonSettings.jvm, publicationSettings.jvm, testSettings.jvm)
-    .settings(
-      libraryDependencies += "org.graalvm.sdk" % "graal-sdk" % Ver.Graal,
-      genExprBoilerplate := GenExprBoilerplate(sourceDirectory.value / "main" / "scala"))
+        extBoopickleJVM, extBoopickleJS)
 
   lazy val extBoopickle = crossProject(JSPlatform, JVMPlatform)
     .in(file("ext-boopickle"))
     .configureCross(commonSettings, publicationSettings, testSettings)
-    .jvmConfigure(_.dependsOn(core))
+//    .jvmConfigure(_.dependsOn(core))
     .settings(
       name := "ext-boopickle",
       libraryDependencies += "io.suzaku" %%% "boopickle" % Ver.BooPickle)
-    .jsSettings(test := (())) // https://github.com/scala-js/scala-js/issues/3673
+    .jsSettings(libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.14.0" % Test)
 
   lazy val extBoopickleJS  = extBoopickle.js
   lazy val extBoopickleJVM = extBoopickle.jvm
     .settings(unmanagedResources in Test += (fastOptJS in Test in extBoopickleJS).value.data)
-
-  lazy val extPrometheus = project
-    .in(file("ext-prometheus"))
-    .configure(commonSettings.jvm, publicationSettings.jvm, testSettings.jvm)
-    .dependsOn(core)
-    .settings(
-      name := "ext-prometheus",
-      libraryDependencies += "io.prometheus" % "simpleclient" % Ver.Prometheus)
-
-  lazy val benchmark = project
-    .configure(commonSettings.jvm, preventPublication)
-    .enablePlugins(JmhPlugin)
-    .dependsOn(core, extBoopickleJVM)
-    .settings(unmanagedResources in Compile += (fullOptJS in Test in extBoopickleJS).value.data)
 }
