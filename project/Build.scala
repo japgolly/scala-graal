2e3586af16e982553e132b2d57d9109b88b516d4
Scala Steward <me@scala-steward.org>
2020-05-11 16:57:35 +0200

    Update simpleclient to 0.9.0

--- project/Build.scala
+++ project/Build.scala
@@ -30,7 +30,7 @@ object ScalaGraal {
     val MonadicFor      = "0.3.1"
     val MTest           = "0.7.1"
     val Nyaya           = "0.9.0"
-    val Prometheus      = "0.8.1"
+    val Prometheus      = "0.9.0"
     val Scala212        = "2.12.11"
     val Scala213        = "2.13.1"
     val ScalaCollCompat = "2.1.4"
