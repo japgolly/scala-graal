466092b1f0a8ed43564a9a52135143b07e1dac78
David Barri <japgolly@gmail.com>
2020-05-20 10:21:49 +1000

    Upgrade deps

--- project/Build.scala
+++ project/Build.scala
@@ -26,14 +26,14 @@ object ScalaGraal {
     val Cats            = "2.1.1"
     val Graal           = "20.0.0"
     val KindProjector   = "0.11.0"
-    val Microlibs       = "2.0"
+    val Microlibs       = "2.3"
     val MonadicFor      = "0.3.1"
     val MTest           = "0.7.1"
-    val Nyaya           = "0.9.0"
-    val Prometheus      = "0.8.1"
+    val Nyaya           = "0.9.2"
+    val Prometheus      = "0.9.0"
     val Scala212        = "2.12.11"
-    val Scala213        = "2.13.1"
-    val ScalaCollCompat = "2.1.4"
+    val Scala213        = "2.13.2"
+    val ScalaCollCompat = "2.1.6"
   }
 
   def scalacFlags = Seq(
