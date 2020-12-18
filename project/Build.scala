2ab70024bd04d8861d91098b90c556d247f62edc
Scala Steward <me@scala-steward.org>
2020-12-18 01:20:21 +0100

    Update cats-core to 2.3.0

--- project/Build.scala
+++ project/Build.scala
@@ -23,7 +23,7 @@ object ScalaGraal {
 
   object Ver {
     val BooPickle       = "1.3.3"
-    val Cats            = "2.1.1"
+    val Cats            = "2.3.0"
     val Graal           = "20.3.0"
     val KindProjector   = "0.11.2"
     val Microlibs       = "2.5"
