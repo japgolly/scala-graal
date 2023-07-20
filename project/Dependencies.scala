f6949f7086ea8207d29c3f0034e3f2b36855f71d
Scala Steward <scala_steward@virtuslab.com>
2023-07-20 16:57:47 +0000

    Update graal-sdk to 21.3.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     // Exported
     val boopickle        = "1.4.0"
     val cats             = "2.8.0"
-    val graal            = "21.3.1"
+    val graal            = "21.3.7"
     val microlibs        = "4.2.1"
     val prometheus       = "0.16.0"
     val scala2           = "2.13.9"
